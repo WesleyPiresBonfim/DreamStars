542c07ccf994c411b61b82e64bbbbbefee861cab
WesleyPiresBonfim <42045631+WesleyPiresBonfim@users.noreply.github.com>
2024-10-08 10:19:46 +0100

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -1,45 +1,39 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-        desc = { enumerable: true, get: function() { return m[k]; } };
+
+const Client = require("./bot/structure/Client.js");
+const fastify = require("fastify")({ logger: true });
+const dotenv = require("dotenv");
+
+// Carregar o arquivo .env
+dotenv.config();
+
+// Verifique se o token está definido
+if (!process.env.TOKEN) {
+    console.error("Erro: O token do bot não está definido. Verifique o arquivo .env");
+    process.exit(1);
+}
+
+// Inicie o Fastify e defina um endpoint simples
+fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+});
+
+fastify.listen(3000, "0.0.0.0", (err, address) => {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
     }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Oceanic = __importStar(require("oceanic.js"));
-const Client_js_1 = require("./bot/structure/Client");
-
-const client = new Client_js_1.Client({
-    token: process.env.TOKEN,
-    intents: ["GUILDS", "GUILD_MESSAGES"],
+    fastify.log.info(`Server is running on ${address}`);
 });
 
-// Aqui é onde você deve chamar o método load do cliente
-client.load = async () => {
-    // Lógica de carregamento, se necessário
+// Carregar e conectar o bot
+Client.bot.load().then(() => {
     console.log("Bot carregado com sucesso!");
-};
-
-client.connect().then(() => {
-    console.log("Bot conectado!");
-}).catch(console.error);
+    Client.bot.connect().then(() => {
+        console.log("Bot conectado!");
+    }).catch((err) => {
+        console.error("Erro ao conectar o bot:", err);
+    });
+}).catch((err) => {
+    console.error("Erro ao carregar o bot:", err);
+});
