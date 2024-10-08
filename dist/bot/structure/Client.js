98cd00673829c02b047f075f9cd2a16cc81ef219
WesleyPiresBonfim <42045631+WesleyPiresBonfim@users.noreply.github.com>
2024-10-08 11:16:30 +0100

    Add files via upload

--- dist/bot/structure/Client.js
+++ dist/bot/structure/Client.js
@@ -1,69 +1,74 @@
 "use strict";
-
-const { Client: OceanicClient } = require("oceanic.js");
-const path = require("path");
-const fs = require("fs");
-
-class Client extends OceanicClient {
-    constructor(options) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bot = exports.Client = void 0;
+const path_1 = __importDefault(require("path"));
+const Oceanic = __importStar(require("oceanic.js"));
+const CommandManager_js_1 = require("../managers/CommandManager.js");
+const env_js_1 = require("../../env.js");
+class Client extends Oceanic.Client {
+    command = new CommandManager_js_1.CommandManager(this);
+    constructor() {
         super({
-            auth: process.env.TOKEN,  // Use o token do .env
-            gateway: {
-                intents: [
-                    "GUILDS",
-                    "GUILD_MESSAGES",
-                    "MESSAGE_CONTENT",
-                ]
-            }
+            auth: `Bot ${env_js_1.enhancer.env.TOKEN}`,
         });
-
-        this.commands = [];
-        this.slashCommands = [];
     }
-
-    // Método para carregar todos os comandos
+    mappingSlash(commands, subcommand) {
+        const command = commands.map((i) => {
+            return {
+                ...i,
+                type: subcommand && i.type == 'group' ? 2 : 1,
+                options: i.type == 'command'
+                    ? Object.values(i.args).map((i) => {
+                        return {
+                            ...i,
+                            autocomplete: i.autocomplete
+                                ? true
+                                : undefined,
+                        };
+                    })
+                    : this.mappingSlash(i.commands, true),
+            };
+        });
+        return command;
+    }
+    bulkEditGlobalCommands() {
+        return this.application.bulkEditGlobalCommands(this.mappingSlash(this.command.commands));
+    }
+    bulkEditGuildCommands(guild) {
+        return this.application.bulkEditGuildCommands(guild, this.mappingSlash(this.command.commands));
+    }
     async load() {
-        try {
-            const commandPath = path.join(__dirname, "../commands");
-            const files = fs.readdirSync(commandPath);
-
-            for (const file of files) {
-                const command = require(path.join(commandPath, file));
-                this.commands.push(command);
-                console.log(`Comando ${command.name} carregado com sucesso!`);
-            }
-
-            this.mappingSlash();
-            await this.bulkEditGlobalCommands();
-        } catch (err) {
-            console.error("Erro ao carregar comandos:", err);
-        }
-    }
-
-    // Mapeia os comandos em formato de slash commands
-    mappingSlash() {
-        this.slashCommands = this.commands.map((command) => ({
-            name: command.name,
-            description: command.description,
-            options: command.args ? Object.values(command.args).map((arg) => ({
-                name: arg.name,
-                description: arg.description,
-                type: arg.type,
-                required: arg.required || false,
-            })) : []
-        }));
-    }
-
-    // Adiciona os comandos de barra globais para o bot
-    async bulkEditGlobalCommands() {
-        try {
-            const commands = await this.application.bulkEditGlobalCommands(this.slashCommands);
-            console.log("Comandos globais registrados: ", commands);
-        } catch (err) {
-            console.error("Erro ao registrar comandos globais:", err);
-        }
+        await this.command.loadCommand(path_1.default.resolve(__dirname, '../', 'commands'));
+        await this.command.loadArgumentsFromCommands(this.command.commands);
+        await this.restMode();
+        return this;
     }
 }
-
-// Exporta a instância do bot
-module.exports = { bot: new Client() };
+exports.Client = Client;
+exports.bot = new Client();
