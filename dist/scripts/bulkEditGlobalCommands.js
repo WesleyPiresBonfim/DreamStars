918f49c80714712d9da15e37ac5be1fa86ea5dea
WesleyPiresBonfim <42045631+WesleyPiresBonfim@users.noreply.github.com>
2024-10-08 12:24:46 +0100

    Add files via upload

--- dist/scripts/bulkEditGlobalCommands.js
+++ dist/scripts/bulkEditGlobalCommands.js
@@ -3,10 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Client_1 = require("../bot/structure/Client");
 const Logging_1 = require("../modules/Logging");
 require("../index");
+
 const initTime = Date.now();
+
 Client_1.bot.on('ready', () => {
     Client_1.bot.bulkEditGlobalCommands().then(() => {
-        (0, Logging_1.log)(`Bulk editing of commands completed successfully in ${((Date.now() - initTime) /
-            1000).toFixed(2)} seconds`, ['bot:bulkEditGlobalCommands'.magenta]);
+        (0, Logging_1.log)(`Bulk editing of commands completed successfully in ${((Date.now() - initTime) / 1000).toFixed(2)} seconds`, ['bot:bulkEditGlobalCommands'.magenta]);
     });
 });
