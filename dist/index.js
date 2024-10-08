d2803b0b04d5f3575f1923fce84383951701edd0
WesleyPiresBonfim <42045631+WesleyPiresBonfim@users.noreply.github.com>
2024-10-08 11:41:44 +0100

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,35 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Logging_1 = require("./modules/Logging");
+const server_1 = require("./http/server");
+const env_1 = require("./env");
+const Client_1 = require("./bot/structure/Client");
 
-const Client = require("./bot/structure/Client.js");
-const fastify = require("fastify")({ logger: true });
-const dotenv = require("dotenv");
-
-// Carregar o arquivo .env
-dotenv.config();
-
-// Verifique se o token está definido
-if (!process.env.TOKEN) {
-    console.error("Erro: O token do bot não está definido. Verifique o arquivo .env");
-    process.exit(1);
-}
-
-// Inicie o Fastify e defina um endpoint simples
-fastify.get("/", async (request, reply) => {
-    return { hello: "world" };
-});
-
-fastify.listen(3000, "0.0.0.0", (err, address) => {
-    if (err) {
-        fastify.log.error(err);
-        process.exit(1);
+(async () => {
+    // Log de ambiente
+    if (env_1.enhancer.env.NAME != 'production') {
+        (0, Logging_1.warn)(`Environment: ${env_1.enhancer.env.NAME.magenta}`, [
+            'app:environment'.blue,
+        ]);
     }
-    fastify.log.info(`Server is running on ${address}`);
-});
 
-// Carregar e conectar o bot
-Client.bot.load().then(() => {
-    console.log("Bot carregado com sucesso!");
-    Client.bot.connect().then(() => {
-        console.log("Bot conectado!");
-    }).catch((err) => {
-        console.error("Erro ao conectar o bot:", err);
+    // Carregar o bot
+    await Client_1.bot.load();
+
+    // Evento quando o bot estiver pronto
+    Client_1.bot.on('ready', () => {
+        (0, Logging_1.log)('Bot is now ' + 'Ready'.magenta + '!', [
+            'bot:gatewayConnection'.blue,
+        ]);
+
+        // Sincronizar comandos globais apenas em produção
+        if (env_1.enhancer.env.NAME == 'production') {
+            Client_1.bot.bulkEditGlobalCommands();
+        }
     });
-}).catch((err) => {
-    console.error("Erro ao carregar o bot:", err);
-});
+
+    // Inicialização do servidor HTTP
+    server_1.app.listen({ host: '0.0.0.0', port: env_1.enhancer.env.PORT }).then((i) => {
+        (0, Logging_1.log)('Server is running', ['fastify:server'.green]);
+    });
+})();
